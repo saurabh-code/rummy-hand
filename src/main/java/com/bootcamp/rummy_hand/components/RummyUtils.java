656ccefd0d5905897743d081438926b05ce47b38
saurabh-code <saurabh.kumar.code@gmail.com>
2018-08-10 16:42:43 +0530

    utils struct

---
+++ src/main/java/com/bootcamp/rummy_hand/components/RummyUtils.java
@@ -0,0 +1,21 @@
+package com.bootcamp.rummy_hand.components;
+
+import java.util.List;
+import java.util.Map;
+
+import com.bootcamp.rummy_hand.card.Card;
+
+public class RummyUtils {
+
+	public static Map<String, List<Card>> createRunMaps(List<Card> hand) {
+		return null;
+	};
+	
+	public static Map<String, List<Card>> createSetMaps(List<Card> hand) {
+		return null;
+	}
+	
+	public static List<List<Card>> getAllValidRuns(Map<String, List<Card>> runMaps) {
+		return null;
+	}
+}
